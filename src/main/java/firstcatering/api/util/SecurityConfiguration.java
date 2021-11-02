3bedb916bd02e3195ceb15966c816ffd274bdeb2
Mariana Stoiovici <mariana.stoiovici@gmail.com>
2021-11-02 09:30:03 +0000

    refactored configuration

--- src/main/java/firstcatering/api/util/SecurityConfiguration.java
+++ src/main/java/firstcatering/api/util/SecurityConfiguration.java
@@ -3,18 +3,27 @@ package firstcatering.api.util;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Autowired
   public DataSource dataSource;
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder(){
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
